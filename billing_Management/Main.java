549f8590af3a8503f71c20b58be44b1d6870ea1b
Bayezid Bin Zahid <73067441+Bayezid-Prince@users.noreply.github.com>
2023-05-24 15:10:40 +0600

    Add files via upload

---
+++ billing_Management/Main.java
@@ -0,0 +1,14 @@
+package codes;
+import java.awt.Color;
+import java.awt.Dimension;
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+public class Main {
+
+	public static void main(String[] args) {		
+		
+		MainWindow sua= new MainWindow();
+		sua.showWindow(null, 0);
+	}
+}
+
